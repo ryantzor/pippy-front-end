974a5cdc8c435e221451c61f318684645df2c80c
Ryan Tuller <ryantulleris@gmail.com>
2022-01-08 16:58:55 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from 'react'
+const Google = require('google-api-wrapper')
 
-function App() {
+import logo from './pippy.png';
+import './App.css';
+import token from './token.json'
+
+const App = () => {
+  const hiddenFileInput  = useRef(null);
+
+  const handleFileUploadClick = event => {
+    hiddenFileInput.current.click();
+  }
+
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    console.log('FILE HERE', fileUploaded);
+    // upload file to the google drive api;
+  }
+
+  // runs to auth google
+  useEffect(async () => {
+    
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ fontWeight: 200 }}>
+          insert .mp3 files below
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-container-upload-btn">
+          <button 
+            className="App-upload-btn"
+            onClick={handleFileUploadClick}>
+            Click here to insert meme
+          </button>
+          <input
+            type="file"
+            ref={hiddenFileInput}
+            onChange={handleChange}
+            style={{ display: 'none' }}>
+          </input>
+        </div>
       </header>
     </div>
   );
